305d12f5cba58f600e2db49896c20c29257798ed
akimoto <kurikiton.max@gmail.com>
2019-05-07 02:09:10 +0000

    Finish basic information

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :logged_in_user, only: [:index, :edit, :update]
   before_action :correct_user,   only: [:edit, :update]
-  before_action :admin_user,     only: :destroy
+  before_action :admin_user,     only: [:destroy, :edit_basic_info, :update_basic_info]
   
   def index
     @users = User.paginate(page: params[:page])
@@ -43,33 +43,26 @@ def destroy
       render 'edit'
     end
   end
-
-   private
-
-    def user_params
-      params.require(:user).permit(:name, :email, :password,:password_confirmation)
-    end
-
-    # beforeアクション
-
-    # ログイン済みユーザーか確認
-    def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger] = "ログインしてください。"
-        redirect_to login_url
-      end
-    end
-
-    # 正しいユーザーかどうか確認
-    def correct_user
-      @user = User.find(params[:id])
-      redirect_to(root_url) unless current_user?(@user)
-    end
-
-
-  # 管理者かどうか確認
-    def admin_user
-      redirect_to(root_url) unless current_user.admin?
-    end
+  
+def edit_basic_info
+  @user = User.find(params[:id])
 end
+
+def update_basic_info
+  @user = User.find(params[:id])
+  if @user.update_attributes(basic_info_params)
+    flash[:success] = "基本情報を更新しました。"
+    redirect_to @user   
+  else
+    render 'edit_basic_info'
+  end
+end
+
+private
+  def user_params
+    params.require(:user).permit(:name, :email, :department, :password, :password_confirmation)
+  end
+
+  def basic_info_params
+    params.require(:user).permit(:basic_time, :work_time)
+  end
