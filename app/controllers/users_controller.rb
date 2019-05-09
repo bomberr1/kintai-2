9ed5c639b9f7a8a951ccbce69e56bcdf55802fce
akimoto <kurikiton.max@gmail.com>
2019-05-09 03:58:07 +0000

    Finish Attendances Show Page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,30 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:index, :edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user,   only: [:edit, :update]
-  before_action :admin_user,     only: [:destroy, :edit_basic_info, :update_basic_info]
-  
-  def index
+
+   def index
     @users = User.paginate(page: params[:page])
   end
 
-  def show
-    @user = User.find(params[:id])
+def show
+  @user = User.find(params[:id])
+  if params[:first_day].nil?
+    @first_day = Date.today.beginning_of_month
+  else
+    @first_day = Date.parse(params[:first_day])
   end
+  @last_day = @first_day.end_of_month
+  (@first_day..@last_day).each do |day|
+    unless @user.attendances.any? {|attendance| attendance.worked_on == day}
+      record = @user.attendances.build(worked_on: day)
+      record.save
+    end
+  end
+    @dates = @user.attendances.where('worked_on >= ? and worked_on <= ?', @first_day, @last_day).order('worked_on')
+ @worked_sum = @dates.where.not(started_at: nil).count
+end
+
+  
 
   def new
     @user = User.new
@@ -26,12 +41,6 @@ class UsersController < ApplicationController
     end
   end
 
-def destroy
-    User.find(params[:id]).destroy
-    flash[:success] = "削除しました。"
-    redirect_to users_url
-  end
-
   def edit
   end
 
@@ -43,7 +52,7 @@ def destroy
       render 'edit'
     end
   end
-  
+
 def edit_basic_info
   @user = User.find(params[:id])
 end
@@ -65,4 +74,25 @@ private
 
   def basic_info_params
     params.require(:user).permit(:basic_time, :work_time)
+  end
+    # beforeアクション
+
+    # ログイン済みユーザーか確認
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "ログインしてください。"
+        redirect_to login_url
+      end
+    end
+
+    # 正しいユーザーかどうか確認
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
+
+
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
+    end
   end
